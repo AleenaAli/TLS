08a807e447c57beb8945e9c66b6fec6d3f595cbd
alinaali12 <52271322+alinaali12@users.noreply.github.com>
2022-06-10 13:45:00 +0500

    Add project files.

---
+++ POCFunctionToTestTLS/ClientResult.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCFunctionToTestTLS
+{
+    //
+    // Summary:
+    //     Typed HTTP result wrapper for all http requests.
+    //
+    // Type parameters:
+    //   T:
+    //     The data return type.
+    public class ClientResult<T> 
+    {
+        //
+        // Summary:
+        //     The result from the request.
+        public string ResultContent;
+
+        //
+        // Summary:
+        //     The strongly typed data returned in the HTTP request.
+        public T Data;
+
+        //
+        // Summary:
+        //     Error message if something went wrong.
+        public string ExceptionText;
+
+        //
+        // Summary:
+        //     The HTTP method used in the request.
+        public HttpMethod HttpMethod
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     The HttpStatusCode returned in the result.
+        public HttpStatusCode StatusCode
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     Indicates if the request completed successfully.
+        public bool IsSuccessStatusCode
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ POCFunctionToTestTLS/SysnDataClient.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCFunctionToTestTLS
+{
+    public class SysnDataClient
+    {
+
+        /// <summary>
+        ///  Gets documents from SynsData based on the Url passed
+        /// </summary>
+        /// <param name="url">URL String sent as a Parameter</param>
+        public async Task<ClientResult<HttpResponseMessage>> GetDocumentData(string url)
+        {
+            var getDocumentDataResult = new ClientResult<HttpResponseMessage>();
+            //var hash = X509Certificate.CreateFromSignedFile("31075939.cer").GetCertHashString();
+            //ServicePointManager.ServerCertificateValidationCallback = delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+            try
+            {
+                using (var httpClientHandler = new HttpClientHandler())
+                {
+
+                    //httpClientHandler.ServerCertificateCustomValidationCallback += (message, cert, chain, sslPolicyErrors) => {
+                    //    if (cert.GetName().Equals("31075939.cer") || cert.Thumbprint== "49225e297d27c21049383aefa2302496e8b364de")
+                    //    {
+                    //        return true;
+                    //    }
+                    //    return true;
+                    //};
+                    var certFile = Path.Combine("C:\\Users\\extcikaal\\Documents\\FDMCertificates\\PFX", "31075939.cer");
+                    httpClientHandler.ClientCertificates.Add(new X509Certificate2(certFile));
+                    //certFile = Path.Combine("C:\\Users\\extcikaal\\Documents\\FDMCertificates\\PFX", "30955730.cer");
+                    //httpClientHandler.ClientCertificates.Add(new X509Certificate2(certFile));
+
+                    ////Setting TLS 1.2 protocol
+                    httpClientHandler.SslProtocols = System.Security.Authentication.SslProtocols.Tls13;
+
+                    HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+                    result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+                    //var stream = new FileStream(url, FileMode.Open, FileAccess.Read);
+                    Uri absoluteUri = new Uri(url, UriKind.Absolute);
+
+                    HttpClient client = new HttpClient(httpClientHandler);
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    Byte[] bytes = await response.Content.ReadAsByteArrayAsync();
+                    //result.Content = new StreamContent(stream);
+
+                    getDocumentDataResult.StatusCode = response.StatusCode;
+                    getDocumentDataResult.ResultContent = bytes.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString());\
+                getDocumentDataResult.ExceptionText = ex.Message;
+                getDocumentDataResult.StatusCode = HttpStatusCode.InternalServerError;
+            }
+            return getDocumentDataResult;
+        }
+
+        public async Task<byte[]> DownloadFile(string url)
+        {
+            try
+            {
+                using (var httpClientHandler = new HttpClientHandler())
+                {
+                    using (var client = new HttpClient(httpClientHandler))
+                    {
+                        var certFile = Path.Combine("C:\\Users\\extcikaal\\Documents\\FDMCertificates\\PFX", "31075939.cer");
+                        httpClientHandler.ClientCertificates.Add(new X509Certificate2(certFile));
+
+                        using (var result = await client.GetAsync(url))
+                        {
+                            if (result.IsSuccessStatusCode)
+                            {
+                                return await result.Content.ReadAsByteArrayAsync();
+                            }
+
+                        }
+                    }
+                }
+                return null;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
